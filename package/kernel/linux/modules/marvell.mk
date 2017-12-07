afa3f2be280999a69212d4bd5e8120f9b75c1d94
Damir Samardzic <damir.samardzic@sartura.hr>
2017-12-07 13:02:07 +0000

    musdk: sysfs kmod
    Signed-off-by: Damir Samardzic <damir.samardzic@sartura.hr>

--- package/kernel/linux/modules/marvell.mk
+++ package/kernel/linux/modules/marvell.mk
@@ -7,3 +7,12 @@ endef
 
 $(eval $(call KernelPackage,crypto-safexcel))
 
+define KernelPackage/mvpp2x-sysfs
+  TITLE:=Marvell Gigabit Ethernet driver
+  SUBMENU:=Marvell
+  KCONFIG:=CONFIG_MVPP2X_SYSFS
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/marvell/mvpp2x/sysfs/mvpp2x_sysfs.ko
+endef
+
+$(eval $(call KernelPackage,mvpp2x-sysfs))
+
